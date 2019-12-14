0a9af58ec0076a11e3b0de0e1030406dbbaf6759
McToluene <xclusiveniggaz10@live.com>
2019-12-14 11:29:45 +0100

    Merge branch 'master' of https://github.com/McToluene/Givrish into Phase
    # Conflicts:
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/givrish/models/UserLoginModel.java
@@ -0,0 +1,11 @@
+package com.example.givrish.models;
+
+public class UserLoginModel {
+  private String phone;
+  private String password;
+
+  public UserLoginModel(String phone, String password) {
+    this.phone = phone;
+    this.password = password;
+  }
+}
