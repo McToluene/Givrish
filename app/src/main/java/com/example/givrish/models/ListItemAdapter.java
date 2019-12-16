aa619cdb021b1d3895c9bf3e7f81a928036bd80d
McToluene <xclusiveniggaz10@live.com>
2019-12-16 10:16:21 +0100

    Item list added to dashboard

---
+++ app/src/main/java/com/example/givrish/models/ListItemAdapter.java
@@ -0,0 +1,57 @@
+package com.example.givrish.models;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.givrish.R;
+
+import java.util.List;
+
+public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ListItemHolder> {
+  private List<ProductModel> productModelList;
+  private  LayoutInflater inflater;
+
+  public ListItemAdapter(List<ProductModel> productModelList, Context context) {
+    this.productModelList = productModelList;
+    inflater = LayoutInflater.from(context);
+  }
+
+  @NonNull
+  @Override
+  public ListItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View view =   inflater.inflate(R.layout.item_card, parent, false);
+    ListItemHolder itemHolder = new ListItemHolder(view);
+    return itemHolder;
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull ListItemHolder holder, int position) {
+    ProductModel product = productModelList.get(position);
+    holder.title.setText(product.getTitle());
+    holder.location.setText(product.getLocation());
+  }
+
+  @Override
+  public int getItemCount() {
+    return productModelList.size();
+  }
+
+  public class ListItemHolder extends RecyclerView.ViewHolder {
+
+    private final TextView title;
+    private final TextView location;
+
+    public ListItemHolder(@NonNull View itemView) {
+      super(itemView);
+
+      title = itemView.findViewById(R.id.tv_title);
+      location = itemView.findViewById(R.id.tv_location);
+    }
+  }
+}
